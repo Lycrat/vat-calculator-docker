1f234d57447ea6bbf1f529cc0314089d573d69d9
Panya <panya.rattanarom@sky.uk>
2025-10-22 15:30:15 +0100

    MODIFY: add tailwind

--- src/App.js
+++ src/App.js
@@ -1,29 +1,28 @@
-import { useState } from 'react';
-import './App.css';
-import DisplayBlock from './DisplayBlock';
-import PriceEntryField from './PriceEntryField';
-import VatRateField from './VatRateField';
-
+import { useState } from "react";
+import "./App.css";
+import DisplayBlock from "./DisplayBlock";
+import PriceEntryField from "./PriceEntryField";
+import VatRateField from "./VatRateField";
 
 function App() {
- const [netPrice, setNetPrice] = useState(0.0);
+  const [netPrice, setNetPrice] = useState(0.0);
   const [grossPrice, setGrossPrice] = useState(0.0);
   const [vatToPay, setVatToPay] = useState(0.0);
   const [vatRate, setVatRate] = useState(20.0);
 
   const handleNetPriceChange = (price) => {
-    const gross_price = (price * ((vatRate / 100) + 1)).toFixed(2);
+    const gross_price = (price * (vatRate / 100 + 1)).toFixed(2);
     setNetPrice(parseFloat(price.toFixed(2)));
     setGrossPrice(parseFloat(gross_price));
     setVatToPay(parseFloat((gross_price - price).toFixed(2)));
-};
+  };
 
   const handleGrossPriceChange = (price) => {
-    const net_price = (price / ((vatRate / 100) + 1)).toFixed(2);
+    const net_price = (price / (vatRate / 100 + 1)).toFixed(2);
     setNetPrice(parseFloat(net_price));
     setGrossPrice(parseFloat(price.toFixed(2)));
     setVatToPay(parseFloat((price - net_price).toFixed(2)));
-};
+  };
 
   const handleVatRateChanged = (rate) => {
     setVatRate(rate);
@@ -35,17 +34,37 @@ function App() {
   };
 
   return (
-    <div className='header field'>
-      VAT CALCULATOR
-      <div className='colour-border'>
-        <VatRateField customstyle="field" vatRateChanged={handleVatRateChanged} value={vatRate} updatePrices={updatePrices} />
-        <PriceEntryField customstyle="field" label="Price excl VAT: " priceChanged={handleNetPriceChange} price={netPrice === 0.0 ? "" : netPrice} />
-        <DisplayBlock customstyle="field" label="VAT to pay: " value={vatToPay} />
-        <PriceEntryField customstyle="field" label="Price incl VAT: " priceChanged={handleGrossPriceChange} price={grossPrice === 0.0 ? "" : grossPrice} />
+    <div className="flex flex-col gap-5 w-full h-full justify-center items-center">
+      <div className="header field">
+        VAT CALCULATOR
+        <div className="colour-border">
+          <VatRateField
+            customstyle="field"
+            vatRateChanged={handleVatRateChanged}
+            value={vatRate}
+            updatePrices={updatePrices}
+          />
+          <PriceEntryField
+            customstyle="field"
+            label="Price excl VAT: "
+            priceChanged={handleNetPriceChange}
+            price={netPrice === 0.0 ? "" : netPrice}
+          />
+          <DisplayBlock
+            customstyle="field"
+            label="VAT to pay: "
+            value={vatToPay}
+          />
+          <PriceEntryField
+            customstyle="field"
+            label="Price incl VAT: "
+            priceChanged={handleGrossPriceChange}
+            price={grossPrice === 0.0 ? "" : grossPrice}
+          />
+        </div>
       </div>
     </div>
   );
 }
 
 export default App;
-
